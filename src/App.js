19472ae04882e855d3aa13690c1c773c09f0fb23
JulianTM <jpachonl99@gmail.com>
2021-03-19 16:55:42 -0500

    version 1

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
+
+function App() {
+
+  // state del formulario
+  const [busqueda, setBusqueda] = useState({
+    ciudad:'',
+    pais:''
+  });
+  const [consultar, setConsultar] = useState(false);
+  const [resultado, setResultado] = useState({});
+  const [error, setError] = useState(false);
+
+  // extraer los datos
+  const { ciudad, pais} = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+
+      if(consultar){
+        const appId = 'c90436290b8e1abde35aa2ab289910a8';
+        const url= `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        setResultado(resultado);
+        setConsultar(false);
+
+        //Detectar que halla un error, cuando no se encuentra la ciudad
+        if(resultado.cod === "404"){
+          setError(true);
+        }else{
+          setError(false);
+        }
+      }
+    }
+    consultarAPI();
+    // eslint.disable-next-line
+  }, [consultar]);
+
+  // Carga condicional de componentes
+  let componente;
+  if(error){
+    componente = <Error mensaje="No hay resultados" />
+  }else{
+    componente = <Clima
+                  resultado={resultado}
+                  />
+  }
+
+
+  return (
+    <Fragment>
+      <Header 
+        titulo="Clima React App"
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12"> 
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12"> 
+                {componente}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
